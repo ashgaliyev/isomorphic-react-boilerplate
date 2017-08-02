7bc15ea0711fe36c7188f1c826ddd3cdc53b7fb7
Andrey Ashgaliyev <a.ashgaliev@gmail.com>
2017-08-02 13:57:33 +0500

    init

---
+++ webpack.config.js
@@ -0,0 +1,83 @@
+global.Promise = require('bluebird')
+
+var webpack = require('webpack')
+var path = require('path')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var CleanWebpackPlugin = require('clean-webpack-plugin')
+
+var publicPath = 'http://localhost:8050/public/assets'
+var cssName =
+  process.env.NODE_ENV === 'production' ? 'styles-[hash].css' : 'styles.css'
+var jsName =
+  process.env.NODE_ENV === 'production' ? 'bundle-[hash].css' : 'bundle.css'
+
+var plugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      BROWSER: JSON.stringify(true),
+      NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'development'),
+    },
+  }),
+  new ExtractTextPlugin(cssName),
+]
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(
+    new CleanWebpackPlugin(['public/assets/'], {
+      root: __dirname,
+      verbose: true,
+      dry: false,
+    })
+  )
+  plugins.push(new webpack.optimize.OccurrenceOrderPlugin())
+}
+
+module.exports = {
+  entry: ['babel-polyfill', './src/client.js'],
+  resolve: {
+    modules: ['node_modules'],
+    extensions: ['.js', '.jsx'],
+  },
+  plugins,
+  output: {
+    path: `${__dirname}/public/assets/`,
+    filename: jsName,
+    publicPath,
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract(
+          'style-loader',
+          'css-loader!postcss-loader'
+        ),
+      },
+      {
+        test: /\.less$/,
+        loader: ExtractTextPlugin.extract(
+          'style-loader',
+          'css-loader!postcss-loader!less-loader'
+        ),
+      },
+      { test: /\.gif$/, loader: 'url-loader?limit=10000&mimetype=images/gif' },
+      { test: /\.jpg$/, loader: 'url-loader?limit=10000&mimetype=images/jpg' },
+      { test: /\.png$/, loader: 'url-loader?limit=10000&mimetype=images/png' },
+      {
+        test: /\.svg$/,
+        loader: 'url-loader?limit=26000&mimetype=images/svg+xml',
+      },
+      { test: /\.(woff|woff2|ttf|eot)/, loader: 'url-loader?limit=1' },
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader!eslint-loader',
+        exclude: [/node_modules/, /public/],
+      },
+      { test: /\.json$/, loader: 'json-loader' },
+    ],
+  },
+  devtool: process.env.NODE_ENV !== 'production' ? 'eval' : false,
+  devServer: {
+    headers: { 'Access-Control-Allow-Origin': '*' },
+  },
+}
